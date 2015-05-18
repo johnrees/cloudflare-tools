01dee1ead4422f0ce7dc7421b00c73baf4d992d1
John Rees <john@bitsushi.com>
2015-05-18 20:39:52 +0100

    options

--- cf.rb
+++ cf.rb
@@ -8,6 +8,30 @@ require 'json'
 EMAIL = ENV.fetch("EMAIL")
 AUTH_KEY = ENV.fetch("AUTH_KEY")
 
+class String
+  # colorization
+  def colorize(color_code)
+    "\e[#{color_code}m#{self}\e[0m"
+  end
+
+  def red
+    colorize(31)
+  end
+
+  def green
+    colorize(32)
+  end
+
+  def yellow
+    colorize(33)
+  end
+
+  def pink
+    colorize(35)
+  end
+
+end
+
 def get_json url
   JSON.parse(open(
     url,
@@ -22,20 +46,42 @@ end
 zones = get_json("https://api.cloudflare.com/client/v4/zones")
 zones = zones["result"].collect{ |r| {r["name"] => r["id"]} }.inject(:merge)
 
-while (puts zones.keys.sort; print "> "; input = gets) do
-  input.chomp!
-  puts "Input was: #{input.inspect}"
-  case input
-  when *zones.keys then puts get_json("https://api.cloudflare.com/client/v4/zones/#{zones[input]}/dns_records")
+puts zones.keys.sort
+
+while (print "Type a domain > ".yellow; domain = gets) do
+  domain.chomp!
+  puts "Input was: #{domain.inspect}".pink
+  case domain
+  when *zones.keys then break
   when "q", "quit", "exit" then break
-  else puts "I don't know that command"
+  else puts "I don't know that command. Please enter something like #{zones.keys.sample}"
   end
 end
 
-# 2. Ask for command -
-#   Backup DNS records
-#   Restore DNS records
-#   Copy DNS records from another domain
-#   Delete all DNS records
-#   Add Google Apps DNS
+# 2. Ask for an option
 
+options = [
+  'Backup DNS records',
+  'Restore DNS records',
+  'Copy DNS records from another domain',
+  'Delete all DNS records',
+  'Add Google Apps DNS'
+]
+
+puts options.map.with_index(1).map{|q,i| "#{i}) #{q}" }
+
+while (print "Choose an option (1-#{options.length}) > ".yellow; option = gets) do
+  option.chomp!
+  puts "Input was: #{option.inspect}".pink
+  case option
+  when '1' then puts 'backup'
+  when '2' then puts 'restore'
+  when '3' then puts 'copy'
+  when '4' then puts 'delete'
+  when '5' then puts 'google'
+  when "q", "quit", "exit" then break
+  else puts "I don't know that command. Please enter an integer between 1 and #{options.length}"
+  end
+end
+
+# puts get_json("https://api.cloudflare.com/client/v4/zones/#{zones[input]}/dns_records")
